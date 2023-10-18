4eb1b8fbea5269a97722743f8b56980d9fe0fd20
Shocker <zacsmcdowell@gmail.com>
2023-10-18 14:42:23 -0400

    added input prompts and logic for license

--- index.js
+++ index.js
@@ -1,13 +1,53 @@
-// TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const inquirer = require('inquirer')
+
+const licenseChoices = ['MIT', 'Apache-2.0', 'GPL-3.0', 'None']
+
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Project Title:'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Project Description:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Installation:'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Usage:'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'License:',
+        choices: licenseChoices,
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Contributing:'
+    },
+]
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        // Once the user has answered the questions, you can use the answers to generate your README content and write it to a file.
+        const readmeContent = generateMarkdown(answers)
+        writeToFile('README.md', readmeContent)
+    })
+}
 
 // Function call to initialize app
-init();
+init()
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,30 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let badge = ""
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+  if (license != "None") {
+    badge = "![Badge Icon](https://shields.io/badge/license-" + license + "-blue)"
+  }
+
+  return badge
+}
+
+
+function renderLicenseLink(license) {
+  if (license === 'MIT') {
+    return '[MIT License](https://opensource.org/licenses/MIT)'
+  } else if (license === 'Apache-2.0') {
+    return '[Apache License 2.0](https://www.apache.org/licenses/LICENSE-2.0)'
+  } else if (license === 'GPL-3.0') {
+    return '[GPL-3.0 License](https://www.gnu.org/licenses/gpl-3.0)'
+  } else {
+    // Return an empty string if no license is provided
+    return ''
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
